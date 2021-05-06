5336a332bbacda6d8a4533e000fe038eb9d3ae82
Biodun Bamigboye <36827870+mrwaccaff@users.noreply.github.com>
2021-05-06 19:30:28 +0100

    Day 2 Progress

--- src/router.js
+++ src/router.js
@@ -3,27 +3,33 @@ import Router from 'vue-router'
 import EventCreate from './views/EventCreate.vue'
 import EventList from './views/EventList.vue'
 import EventShow from './views/EventShow.vue'
+import EventAdd from './views/EventAdd.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'event-list',
-      component: EventList
-    },
-    {
-      path: '/event/:id',
-      name: 'event-show',
-      component: EventShow,
-      props: true
-    },
-    {
-      path: '/event/create',
-      name: 'event-create',
-      component: EventCreate
-    }
-  ]
+    mode: 'history',
+    routes: [{
+            path: '/',
+            name: 'event-list',
+            component: EventList
+        },
+        {
+            path: '/event/:id',
+            name: 'event-show',
+            component: EventShow,
+            props: true
+        },
+        {
+            path: '/event/create',
+            name: 'event-create',
+            component: EventCreate
+        },
+        {
+            path: '/event/add',
+            name: 'event-add',
+            component: EventAdd,
+        }
+    ]
+
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,42 @@
 <template>
-  <h1>Create Event</h1>
+ <div>
+  <h1>Create Event {{ user.name }}</h1>
+  <p> This event was created by {{ user.id }}</p>
+  <ul>
+   <li v-for="cat in categories" :key="cat">{{cat }}</li>
+  </ul>
+  <p>There are {{catLength }} categories</p>
+  <p>{{ getEventById(3) }}</p>
+  <button v-on:click="INCREMENT_COUNT" >Current Count is {{ count }}</button>
+  <input type="number" v-model.number="incrementer" @change="INCREMENT_COUNT" placeholder="Enter Increment Value">
+ 
+ </div>
 </template>
+<script>
+import { mapState,mapGetters  }  from 'vuex'
+import DatePicker from 'vuejs-datepicker'
+export default{
+  components:{
+    DatePicker
+  },
+  data(){
+    return { 
+      incrementer: 0
+      }
+  },
+computed : {
+  getEventById (){return this.$store.getters.getEventById },
+  catLength() {return this.$store.getters.catLength},
+  
+  ...mapGetters(['getEventById']),
+  ...mapState(['user','categories','count'])
+},
+methods : {
+INCREMENT_COUNT(){
+  this.$store.dispatch('updateCount',this.incrementer)
+},
+}
+
+
+}
+</script>
